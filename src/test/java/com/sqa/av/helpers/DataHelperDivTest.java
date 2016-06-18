27cdaafbc0afeb7dc0e3cf4ef8d30798aecdf7ea
Arunkumar Venkatraman <arunkumar.venkatraman@gmail.com>
2016-06-18 09:33:54 -0700

    Add Div project source code

--- src/test/java/com/sqa/av/helpers/DataHelperDivTest.java
+++ src/test/java/com/sqa/av/helpers/DataHelperDivTest.java
@@ -5,66 +5,64 @@ import org.testng.annotations.*;
 
 import com.sqa.av.helpers.data.*;
 
-public class DataHelperTest {
-	@DataProvider(name = "textData")
-	public Object[][] getData() {
-		Object[][] data;
-		data = DataHelper.getTextFileData("src/main/resources/", "data.csv", TextFormat.CSV, true);
-		DisplayHelper.multArray(data);
-		return data;
-	}
+public class DataHelperDivTest {
 
-	@DataProvider(name = "textDataTyped")
+	// @DataProvider(name = "textIsDivisible")
+	// public Object[][] getData() {
+	// Object[][] data;
+	// data = DataHelper.getTextFileData("src/main/resources/", "dataDiv.csv",
+	// TextFormat.CSV, true);
+	// DisplayHelper.multArray(data);
+	// return data;
+	// }
+
+	@DataProvider(name = "textIsDivisibleTyped")
 	public Object[][] getDataTyped() {
 		Object[][] data;
-		data = DataHelper.getTextFileData("src/main/resources/", "data.csv", TextFormat.CSV, true, Integer.TYPE,
-				Boolean.TYPE);
-		DisplayHelper.multArray(data);
-		return data;
-	}
-
-	@DataProvider(name = "textDataTypedWithString")
-	public Object[][] getDataTypedWithString() {
-		Object[][] data;
-		data = DataHelper.getTextFileData("src/main/resources/", "data2.csv", TextFormat.CSV, true, String.class,
+		data = DataHelper.getTextFileData("src/main/resources/", "dataDiv.csv", TextFormat.CSV, true, Integer.TYPE,
 				Integer.TYPE, Boolean.TYPE);
 		DisplayHelper.multArray(data);
 		return data;
 	}
 
-	@Test(dataProvider = "textData")
-	public void testReadingFile(String number, String isPrime) {
-		try {
-			System.out.println("Number " + number + ", is Prime? (" + isPrime + ")");
-			boolean actualResult = isPrime(Integer.parseInt(number));
-			Assert.assertEquals(actualResult, Boolean.parseBoolean(isPrime), "Number is not prime based on data set.");
+	// @Test(dataProvider = "textIsDivisible")
+	// public void testReadingFile(String dividend, String divisor, String
+	// isDivisible) {
+	// System.out.println("==============================");
+	// System.out.println("Actual Result:");
+	// System.out.println("==============================");
+	// try {
+	// System.out.println("Is the number " + dividend + ", divisible by " +
+	// divisor + "?: " + isDivisible);
+	// boolean actualResult = isDivisible(Integer.parseInt(dividend),
+	// Integer.parseInt(divisor));
+	// Assert.assertEquals(actualResult, Boolean.parseBoolean(isDivisible),
+	// "The Number " + dividend + "is not divisible by " + divisor);
+	// } catch (NumberFormatException e) {
+	// e.printStackTrace();
+	// }
+	// }
 
+	@Test(dataProvider = "textIsDivisibleTyped")
+	public void testReadingFileTyped(int dividend, int divisor, boolean Div) {
+		try {
+			System.out
+					.println("Is the number " + dividend + ", divisible by " + divisor + "? Actual Result --> " + Div);
+			boolean actualResult = isDivisible(dividend, divisor);
+
+			Assert.assertEquals(Div, actualResult,
+					"Expected Result --> The Number " + dividend + " is infact not divisible by " + divisor + ".");
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
+
 	}
 
-	@Test(dataProvider = "textDataTyped")
-	public void testReadingFileTyped(int number, boolean isPrime) {
-		System.out.println("Number " + number + ", is Prime? (" + isPrime + ")");
-		boolean actualResult = isPrime(number);
-		Assert.assertEquals(actualResult, isPrime, "Number is not prime based on data set.");
-	}
-
-	@Test(dataProvider = "textDataTypedWithString")
-	public void testReadingFileTypedWithString(String title, int number, boolean isPrime) {
-		System.out.println("Number " + number + ", is Prime? (" + isPrime + ")");
-		boolean actualResult = isPrime(number);
-		Assert.assertEquals(actualResult, isPrime, "Number is not prime based on data set.");
-	}
-
-	private boolean isPrime(int number) {
-		boolean isPrime = true;
-		for (int i = 2; i <= number / 2; i++) {
-			if (number % i == 0) {
-				isPrime = false;
-			}
-		}
-		return isPrime;
+	private boolean isDivisible(int dividend, int divisor) {
+		boolean isDiv = true;
+		if ((dividend % divisor) == 0)
+			return isDiv = true;
+		else
+			return isDiv = false;
 	}
 }
